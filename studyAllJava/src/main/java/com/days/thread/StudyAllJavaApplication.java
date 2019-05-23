82f664c4034c2ea5e2b2708df8e934c6e8140329
junzizhibang <1213192761@qq.com>
2019-05-23 09:39:35 +0800

    新建SpringBoot工程，主要是开始第一步高级JAVA工程师和架构师知识点总结

---
+++ studyAllJava/src/main/java/com/days/thread/StudyAllJavaApplication.java
@@ -0,0 +1,13 @@
+package com.days.thread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyAllJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyAllJavaApplication.class, args);
+	}
+
+}
