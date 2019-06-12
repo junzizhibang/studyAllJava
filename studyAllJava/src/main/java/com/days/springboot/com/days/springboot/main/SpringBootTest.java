274f9ccb1a2c234e1ada918a7914d3da6ae94341
junzizhibang <1213192761@qq.com>
2019-06-12 23:03:12 +0800

    代码更新下

--- studyAllJava/src/main/java/com/days/springboot/com/days/springboot/main/SpringBootTest.java
+++ studyAllJava/src/main/java/com/days/springboot/com/days/springboot/main/SpringBootTest.java
@@ -1,14 +1,18 @@
 /******************************************** 
  * 模块名称: 工作三年经验，目标是成为JAVA高级开发攻城狮/Java 架构师我在努力！我能行！
  * 软件版权: 马琳-君子自强，脚踏实地积累
- * 源程序名：package-info.java
+ * 源程序名：SpringBootTest.java
  * 功能说明: 俩年迷惘期，回首时才发现自己想要的目标和清晰的职业规划
  * 开发人员: 马琳-君子自强，脚踏实地积累
- * 开发时间: 2019年6月12日 上午8:21:37  今年一定要成为Java高级开发攻城狮 
+ * 开发时间: 2019年6月12日 下午9:16:38  今年一定要成为Java高级开发攻城狮 
  * 相关文档: 基础积累，不同的Java高阶成长路线图
  * 修改记录: 修改日期    修改人员    修改说明
  * v1.0.0.0 2019年6月12日-01    马琳        初始版本基础夯实积攒Java架构师的基础内容  
  *******************************************/
+
+package com.days.springboot.main;
+
+import org.junit.Test;
 
 /**
  *  功能说明：              
@@ -18,10 +22,19 @@
  * @email 1217575485@qq.com
  * 
  */
-package com.days.beans;
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class SpringBootTest {
+	
+	
+	/**
+	*@功能描述        
+	*@作者      马琳-脚踏实地积累。Java进阶
+	*@email    1217575485@qq.com
+	*/
+	 @Test
+	private void mainTest() {
+	 
+	}
 
-
-
-
-
-
+}
